f4e778d105d2d1d0c7980f5a6821f4de566f5658
estrandv <emil.strandvik@gmail.com>
2025-04-12 10:04:02 +0200

    bits

--- src/parser.c
+++ src/parser.c
@@ -935,7 +935,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_LOWER_PARSER_NAME(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 1,
-      .patch_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
